81ff1eb1985778143b9acfa4f6a33d4ea96e25ea
Tim Meusel <tim@bastelfreak.de>
2025-04-11 16:56:21 +0200

    CI: use new username/password

--- spec/acceptance/redis_spec.rb
+++ spec/acceptance/redis_spec.rb
@@ -3,10 +3,25 @@
 require 'spec_helper_acceptance'
 
 describe 'redis' do
-  it 'works idempotently with no errors' do
+  password = ENV.fetch('ICINGA_REPO_PASSWORD', nil)
+  user = ENV.fetch('ICINGA_REPO_USER', nil)
+  # TODO: the secrets are only required for EL tests, so we can limit the scope further down
+  it 'works idempotently with no errors', if: password && user do
     pp = <<~MANIFEST
+      if $facts['os']['family'] == 'RedHat' {
+        yumrepo { 'icinga-stable-release':
+          descr    => 'ICINGA (stable release for epel)',
+          baseurl  => 'https://packages.icinga.com/subscription/rhel/$releasever/release/',
+          enabled  => 1,
+          gpgcheck => 1,
+          gpgkey   => 'https://packages.icinga.com/icinga.key',
+          username => "#{user}",
+          password => "#{password}",
+          before   => Class['icingadb::redis'],
+        }
+      }
       class { 'icingadb::redis':
-        manage_repos => true,
+        manage_repos => $facts['os']['family'] != 'RedHat',
       }
     MANIFEST
 
