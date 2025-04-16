ec418948d88fdf5cb00a2e75ad201ce83fff9c4b
Tim Meusel <tim@bastelfreak.de>
2025-04-16 17:36:03 +0200

    cleanup spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-# Managed by modulesync - DO NOT EDIT
-# https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
-
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-RSpec.configure do |c|
-  c.suite_configure_facts_from_env = true
-end
+ENV['BEAKER_FACTER_ICINGA_REPO_USER'] = ENV.fetch('ICINGA_REPO_USER', nil)
+ENV['BEAKER_FACTER_ICINGA_REPO_PASSWORD'] = ENV.fetch('ICINGA_REPO_PASSWORD', nil)
 
-configure_beaker(modules: :metadata)
+configure_beaker do |host|
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-mysql')
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-postgresql')
+end
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
