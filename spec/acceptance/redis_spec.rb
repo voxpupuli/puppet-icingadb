48955d9c6b36d170fc4b38df048ff82d170cd875
Lennart Betz <lennart.betz@netways.de>
2025-04-16 18:24:52 +0200

    Replace ENVvars with facts in acceptance test, fix logic

--- spec/acceptance/redis_spec.rb
+++ spec/acceptance/redis_spec.rb
@@ -2,12 +2,13 @@
 
 require 'spec_helper_acceptance'
 
-skip = if (fact('os.family') == 'RedHat') && (fact('os.name') != 'Fedora') && !ENV.fetch('ICINGA_REPO_USER', nil).nil? && !ENV.fetch('ICINGA_REPO_PASSWORD', nil).nil?
+skip = if fact('os.family') == 'RedHat' && fact('os.name') != 'Fedora' && (!fact('icinga_repo_user') || !fact('icinga_repo_password'))
          puts 'Skipping acceptance test on RedHat-like systems, because environment variables ICINGA_REPO_USER and ICINGA_REPO_PASSWORD are missing'
          true
        else
          false
        end
+
 describe 'redis', skip: skip do
   it 'works idempotently with no errors' do
     pp = <<~MANIFEST
