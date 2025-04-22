03d787df94193433c826dc63eafe5da82606369f
Lennart Betz <lbetz@prefork.de>
2025-04-22 17:41:57 +0200

    Move repo credentials to GITHUB_ENV

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,9 +2,6 @@
 
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-ENV['BEAKER_FACTER_ICINGA_REPO_USER'] = ENV.fetch('ICINGA_REPO_USER', nil)
-ENV['BEAKER_FACTER_ICINGA_REPO_PASSWORD'] = ENV.fetch('ICINGA_REPO_PASSWORD', nil)
-
 configure_beaker do |host|
   install_puppet_module_via_pmt_on(host, 'puppetlabs-mysql')
   install_puppet_module_via_pmt_on(host, 'puppetlabs-postgresql')
